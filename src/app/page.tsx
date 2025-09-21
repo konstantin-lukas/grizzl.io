c84f4fbdf1724d3b6b2574efe4e2e969cd4d052c
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-09-21 13:34:31 +0200

    add animation to menu button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <>
-            <p>
+            <p className="mt-20">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore odio temporibus totam! Adipisci
                 doloribus enim ex hic itaque libero, nulla quasi recusandae rem voluptatem! Eligendi ipsa natus qui
                 quidem suscipit.
--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -4,7 +4,11 @@ import { Card } from "@heroui/card";
 import { clsx } from "clsx/lite";
 import { useTheme } from "next-themes";
 import Link from "next/link";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
+import Bars2 from "@components/icons/Bars2";
+import XMark from "@components/icons/XMark";
 
 import useHasMounted from "@hooks/useHasMounted";
 
@@ -12,10 +16,13 @@ export default function Menu() {
     const [isOpen, setIsOpen] = useState(false);
     const { setTheme, resolvedTheme } = useTheme();
     const hasMounted = useHasMounted();
+    const openIconRef = useRef(null);
+    const closeIconRef = useRef(null);
     const headerClass = clsx(
         "w-[100dvw]",
         "h-[100dvh]",
-        "bg-black/30",
+        "bg-white/30",
+        "dark:bg-black/30",
         "fixed",
         "top-0",
         "left-0",
@@ -38,11 +45,38 @@ export default function Menu() {
                     </Link>
                 </nav>
                 <button onClick={() => setTheme(hasMounted && resolvedTheme === "dark" ? "light" : "dark")}>
-                    Toggle darkmode
+                    Toggle
                 </button>
             </header>
-            <button className="fixed top-0 left-0 z-50 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-                Menu
+            <button
+                className="fixed top-4 left-4 z-50 cursor-pointer w-8 h-8"
+                aria-label="Toggle the menu"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <CSSTransition
+                    in={isOpen}
+                    timeout={200}
+                    nodeRef={openIconRef}
+                    classNames="menu-icon-open"
+                    unmountOnExit
+                >
+                    <Bars2
+                        ref={openIconRef}
+                        className="w-full h-full stroke-black dark:stroke-white absolute left-0 top-0"
+                    />
+                </CSSTransition>
+                <CSSTransition
+                    in={!isOpen}
+                    timeout={200}
+                    nodeRef={closeIconRef}
+                    classNames="menu-icon-close"
+                    unmountOnExit
+                >
+                    <XMark
+                        ref={closeIconRef}
+                        className="w-full h-full stroke-black dark:stroke-white absolute left-0 top-0"
+                    />
+                </CSSTransition>
             </button>
         </>
     );
