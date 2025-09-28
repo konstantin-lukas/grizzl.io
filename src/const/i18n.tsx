8d5dd0d229c37a5fb83daf27f58d96ca635a37a9
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-09-28 16:22:49 +0200

    add shadow to flags

--- src/const/i18n.tsx
+++ src/const/i18n.tsx
@@ -4,10 +4,10 @@ import EN from "@component/icon/flag/GB";
 import JA from "@component/icon/flag/JP";
 
 export const LANGUAGES = {
-    en: { name: "English", flag: <EN /> },
-    de: { name: "Deutsch", flag: <DE /> },
-    es: { name: "Español", flag: <ES /> },
-    ja: { name: "日本語", flag: <JA className="shadow-xl" /> },
+    en: { name: "English", flag: <EN className="shadow-sm" /> },
+    de: { name: "Deutsch", flag: <DE className="shadow-sm" /> },
+    es: { name: "Español", flag: <ES className="shadow-sm" /> },
+    ja: { name: "日本語", flag: <JA className="shadow-sm" /> },
 };
 export const LOCALES = Object.keys(LANGUAGES) as (keyof typeof LANGUAGES)[];
 export const [DEFAULT_LOCALE] = LOCALES;
