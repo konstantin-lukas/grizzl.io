abecae1262434e180ad78e318ef85ddf3757b837
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-07 16:08:22 +0200

    add missing test id

--- app/components/nav/ProviderButton.vue
+++ app/components/nav/ProviderButton.vue
@@ -2,15 +2,22 @@
 import { authClient } from "@@/lib/auth-client";
 
 const { provider, icon, color } = defineProps<{ provider: string; icon: string; color: string }>();
+const providerLowerCase = provider.toLowerCase();
 async function signIn() {
     await authClient.signIn.social({
-        provider: provider.toLowerCase(),
+        provider: providerLowerCase,
     });
 }
 </script>
 
 <template>
-    <NavBlockLink :color as="button" :aria-label="`Sign in with ${provider}`" @click="signIn">
+    <NavBlockLink
+        :color
+        as="button"
+        :aria-label="`Sign in with ${provider}`"
+        :data-test-id="`${providerLowerCase}-provider`"
+        @click="signIn"
+    >
         <Icon :name="icon" size="1.25rem" />
         {{ provider }}
     </NavBlockLink>
