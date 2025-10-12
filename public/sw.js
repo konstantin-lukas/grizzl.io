82490de9aa461d512bc4d1eabde2f16cab549c39
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-12 12:06:43 +0200

    update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,7 @@
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open("app-cache").then(cache => {
-            return cache.addAll([
-                "/", // cache homepage
-                "/manifest.json",
-                "/web-app-manifest-192x192.png",
-                "/web-app-manifest-512x512.png",
-            ]);
+            return cache.addAll(["/manifest.json", "/web-app-manifest-192x192.png", "/web-app-manifest-512x512.png"]);
         }),
     );
 });
