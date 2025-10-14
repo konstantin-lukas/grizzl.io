23013ee7f0c81398e068d1ecb1546b44cf9e0906
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-14 15:35:12 +0200

    update sw

--- public/sw.js
+++ public/sw.js
@@ -5,11 +5,3 @@ self.addEventListener("install", event => {
         }),
     );
 });
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            return cachedResponse || fetch(event.request);
-        }),
-    );
-});
