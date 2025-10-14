9eee0841e07a815e43492f381ea195a99b7bdbb4
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-14 13:54:30 +0200

    add social providers for production

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,52 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { genericOAuth } from "better-auth/plugins";
 
+const plugins =
+    process.env.NUXT_PUBLIC_APP_ENV !== "production"
+        ? [
+              genericOAuth({
+                  config: [
+                      {
+                          providerId: "keycloak",
+                          clientId: "test-client",
+                          clientSecret: "test-client-secret",
+                          authorizationUrl:
+                              "http://keycloak.grizzl.localhost/realms/dev-test/protocol/openid-connect/auth",
+                          tokenUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/token",
+                          userInfoUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/userinfo",
+                          scopes: ["openid", "email", "profile"],
+                      },
+                  ],
+              }),
+          ]
+        : undefined;
+
+const socialProviders =
+    process.env.NUXT_PUBLIC_APP_ENV === "production"
+        ? {
+              github: {
+                  clientId: process.env.GITHUB_ID!,
+                  clientSecret: process.env.GITHUB_SECRET!,
+              },
+              twitch: {
+                  clientId: process.env.TWITCH_ID!,
+                  clientSecret: process.env.TWITCH_SECRET!,
+              },
+              reddit: {
+                  clientId: process.env.REDDIT_ID!,
+                  clientSecret: process.env.REDDIT_SECRET!,
+              },
+              discord: {
+                  clientId: process.env.DISCORD_ID!,
+                  clientSecret: process.env.DISCORD_SECRET!,
+              },
+              spotify: {
+                  clientId: process.env.SPOTIFY_ID!,
+                  clientSecret: process.env.SPOTIFY_SECRET!,
+              },
+          }
+        : undefined;
+
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
@@ -13,19 +59,6 @@ export const auth = betterAuth({
             generateId,
         },
     },
-    plugins: [
-        genericOAuth({
-            config: [
-                {
-                    providerId: "keycloak",
-                    clientId: "test-client",
-                    clientSecret: "test-client-secret",
-                    authorizationUrl: "http://keycloak.grizzl.localhost/realms/dev-test/protocol/openid-connect/auth",
-                    tokenUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/token",
-                    userInfoUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/userinfo",
-                    scopes: ["openid", "email", "profile"],
-                },
-            ],
-        }),
-    ],
+    plugins,
+    socialProviders,
 });
