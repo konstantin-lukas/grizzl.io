8717aef18495b8d354a810fb2c44d25296c86c47
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-16 00:35:15 +0200

    debug: build webserver outside of container

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,10 +13,9 @@ const plugins =
                           providerId: "keycloak",
                           clientId: "test-client",
                           clientSecret: "test-client-secret",
-                          authorizationUrl:
-                              "http://keycloak.grizzl.localhost/realms/dev-test/protocol/openid-connect/auth",
-                          tokenUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/token",
-                          userInfoUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/userinfo",
+                          authorizationUrl: "http://localhost:8080/realms/dev-test/protocol/openid-connect/auth",
+                          tokenUrl: "http://localhost:8080/realms/dev-test/protocol/openid-connect/token",
+                          userInfoUrl: "http://localhost:8080/realms/dev-test/protocol/openid-connect/userinfo",
                           scopes: ["openid", "email", "profile"],
                       },
                   ],
