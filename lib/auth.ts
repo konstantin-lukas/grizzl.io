1879ed5f1859b1969906bb798e71704b40b00576
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-15 14:10:27 +0200

    debug: test on localhost

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,7 +14,9 @@ const plugins =
                           clientId: "test-client",
                           clientSecret: "test-client-secret",
                           authorizationUrl:
-                              "http://keycloak.grizzl.localhost/realms/dev-test/protocol/openid-connect/auth",
+                              process.env.NUXT_PUBLIC_APP_ENV === "test"
+                                  ? "http://localhost:8080/realms/dev-test/protocol/openid-connect/auth"
+                                  : "http://keycloak.grizzl.localhost/realms/dev-test/protocol/openid-connect/auth",
                           tokenUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/token",
                           userInfoUrl: "http://keycloak:8080/realms/dev-test/protocol/openid-connect/userinfo",
                           scopes: ["openid", "email", "profile"],
