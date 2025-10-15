5bd5e245391548dafb373c58960b600f04574d5b
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-15 23:24:21 +0200

    debug: global setup trace

--- tests/e2e/global.setup.ts
+++ tests/e2e/global.setup.ts
@@ -6,19 +6,28 @@ export default async function GlobalSetup(config: FullConfig) {
     selectors.setTestIdAttribute(testIdAttribute!);
 
     const browser = await firefox.launch();
+    const context = await browser.newContext();
     const page = await browser.newPage();
 
-    await page.goto(`${baseURL}/signin`);
-    await page.getByTestId("keycloak-provider").click();
-    await page.locator("#username").fill("user");
-    await page.locator("#password").fill("password");
-    await page.locator("#kc-login").click();
+    try {
+        await context.tracing.start({ screenshots: true, snapshots: true });
+        await page.goto(`${baseURL}/signin`);
+        await page.getByTestId("keycloak-provider").click();
+        await page.locator("#username").fill("user");
+        await page.locator("#password").fill("password");
+        await page.locator("#kc-login").click();
 
-    await expect(page).toHaveURL(baseURL!);
+        await expect(page).toHaveURL(baseURL!);
 
-    const context = page.context();
-    await context.clearCookies({ name: "i18n_redirected" });
-    await context.storageState({ path: storageState as string });
+        await context.clearCookies({ name: "i18n_redirected" });
+        await context.storageState({ path: storageState as string });
 
-    await browser.close();
+        await browser.close();
+    } catch (error) {
+        await context.tracing.stop({
+            path: "./test-results/failed-setup-trace.zip",
+        });
+        await browser.close();
+        throw error;
+    }
 }
