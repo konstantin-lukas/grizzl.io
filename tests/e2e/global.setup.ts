028bf5dd8db0a24a20ef4d6f644ff6e4c8d0c35b
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-15 12:54:33 +0200

    debug: setup explicit docker network

--- tests/e2e/global.setup.ts
+++ tests/e2e/global.setup.ts
@@ -1,14 +1,23 @@
 import type { FullConfig } from "@playwright/test";
-import { firefox, selectors } from "@playwright/test";
+import { expect, firefox, selectors } from "@playwright/test";
 
 export default async function GlobalSetup(config: FullConfig) {
-    const { storageState, testIdAttribute } = config.projects[0].use;
+    const { baseURL, storageState, testIdAttribute } = config.projects[0].use;
     selectors.setTestIdAttribute(testIdAttribute!);
 
     const browser = await firefox.launch();
     const page = await browser.newPage();
 
+    await page.goto(`${baseURL}/signin`);
+    await page.getByTestId("keycloak-provider").click();
+    await page.locator("#username").fill("user");
+    await page.locator("#password").fill("password");
+    await page.locator("#kc-login").click();
+
+    await expect(page).toHaveURL(baseURL!);
+
     const context = page.context();
+    await context.clearCookies({ name: "i18n_redirected" });
     await context.storageState({ path: storageState as string });
 
     await browser.close();
