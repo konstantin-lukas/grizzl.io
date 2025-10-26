3ffeb8dfed216acf2b8200c2db5491e52db8cdd3
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-10-26 21:38:14 +0100

    set status code

--- server/api/timer/index.post.ts
+++ server/api/timer/index.post.ts
@@ -1,7 +1,9 @@
+import { HttpStatusCode } from "#shared/enum/status";
 import { TimerPostSchema } from "#shared/schema/timer";
 import { safeParseRequestBody } from "~~/server/utils/schema";
 
 export default defineEventHandler(async event => {
     const { success, data, error } = await safeParseRequestBody(event, TimerPostSchema);
+    setResponseStatus(event, HttpStatusCode.CREATED);
     return { success, data, error };
 });
