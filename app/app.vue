ac32fce04f5bd07850f621ecf49259e912378657
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-11-02 15:42:12 +0100

    fix bug that would prevent setting of zod locale on reload

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import * as locales from "@nuxt/ui/locale";
+import { z } from "zod";
+import * as zodLocales from "zod/locales";
 
 const { locale } = useI18n();
+onMounted(() => {
+    z.config(zodLocales[locale.value]());
+});
 </script>
 
 <template>
