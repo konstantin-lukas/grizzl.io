44ca477ad3036a211ded2f9747f082671b3e45d0
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-11-12 20:14:57 +0100

    fix broken import in unit test

--- tests/unit/shared/utils/result/tryCatch.test.ts
+++ tests/unit/shared/utils/result/tryCatch.test.ts
@@ -1,14 +1,14 @@
-import { request } from "@@/shared/utils/result";
+import { tryCatch } from "@@/shared/utils/result";
 import { expect, it } from "vitest";
 
 it("should catch errors and return them as a value", async () => {
-    const { error, data } = await request(Promise.reject());
+    const { error, data } = await tryCatch(Promise.reject());
     expect(error).not.toBeNull();
     expect(data).toBeNull();
 });
 
 it("should catch errors and return them as a value", async () => {
-    const { error, data } = await request(Promise.resolve(42));
+    const { error, data } = await tryCatch(Promise.resolve(42));
     expect(error).toBeNull();
     expect(data).toBe(42);
 });
