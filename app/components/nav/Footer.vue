d0fd91c17cb16c8aa99b59a0882defd0006ddf6f
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-11-12 21:15:29 +0100

    fix failing e2e test

--- app/components/nav/Footer.vue
+++ app/components/nav/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="relative">
-        <footer class="absolute -top-footer-height h-footer-height" data-test-id="inert-elements">
+    <div class="relative" data-test-id="inert-elements">
+        <footer class="absolute -top-footer-height h-footer-height">
             <nav class="mx-4 flex h-full items-center justify-start gap-4 sm:gap-8">
                 <NuxtLink class="inline-link" to="/legal-notice">{{ $t("footer.legalNotice") }}</NuxtLink>
                 <NuxtLink class="inline-link" to="/privacy-policy">{{ $t("footer.privacyPolicy") }}</NuxtLink>
@@ -8,4 +8,3 @@
         </footer>
     </div>
 </template>
-<script setup lang="ts"></script>
