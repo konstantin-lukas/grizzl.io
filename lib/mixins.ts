64b23b7e63491d89c3ba01ded6f610ab9ba1428f
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-11-21 00:01:56 +0100

    refactor: add data constants to mixins

--- lib/mixins.ts
+++ lib/mixins.ts
@@ -3,7 +3,7 @@ import { boolean, char, timestamp } from "drizzle-orm/pg-core";
 import { customAlphabet } from "nanoid";
 
 export function generateId() {
-    const nanoid = customAlphabet("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz", 16);
+    const nanoid = customAlphabet("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz", ID_LENGTH);
     return nanoid();
 }
 
