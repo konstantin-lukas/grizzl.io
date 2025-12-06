03266cc4c78aa2b81b7368a3ec4a28ce8b8b5e93
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-12-06 09:57:04 +0100

    fix(test): remove hrefs of unfinished features

--- tests/e2e/spec/menu.spec.ts
+++ tests/e2e/spec/menu.spec.ts
@@ -31,10 +31,10 @@ const texts = {
 forEachLocale((locale, texts) => {
     test(`should have links to all services for locale ${locale.language}`, async ({ homePage, goto }) => {
         const links = [
-            { locator: homePage.loc.pollLink, text: texts.poll, href: "/poll" },
-            { locator: homePage.loc.todoLink, text: texts.todo, href: "/todo" },
+            { locator: homePage.loc.pollLink, text: texts.poll, href: "" },
+            { locator: homePage.loc.todoLink, text: texts.todo, href: "" },
             { locator: homePage.loc.timerLink, text: texts.timer, href: "/timer" },
-            { locator: homePage.loc.financeLink, text: texts.finance, href: "/finance" },
+            { locator: homePage.loc.financeLink, text: texts.finance, href: "" },
         ];
 
         await test.step("Check that menu elements do not exist when the menu is closed", async () => {
