4d72fee7ce6168bf2d3884c91b7142fb3acdda80
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-12-06 09:39:09 +0100

    style: add coming soon label to unfinished features

--- app/components/nav/BlockLink.vue
+++ app/components/nav/BlockLink.vue
@@ -1,26 +1,47 @@
 <script setup lang="ts">
 import { NuxtLink } from "#components";
 
-const { as = NuxtLink } = defineProps<{ as?: string | object }>();
+const {
+    as = NuxtLink,
+    disabled = false,
+    dataTestId = undefined,
+    to = undefined,
+} = defineProps<{ as?: string | object; disabled?: boolean; dataTestId?: string; to?: string }>();
 </script>
 
 <template>
-    <component
-        :is="as"
-        class="group relative flex h-full w-full justify-center gap-4 overflow-hidden rounded-full text-back"
-    >
-        <span class="w-full transition-transform group-hover:-translate-y-full group-focus-visible:-translate-y-full">
+    <div class="relative">
+        <component
+            :is="disabled ? 'span' : as"
+            :data-test-id="dataTestId"
+            :to="disabled ? null : to"
+            class="group relative flex h-full w-full justify-center gap-4 overflow-hidden rounded-full text-back select-none"
+            :aria-hidden="disabled ?? null"
+        >
             <span
-                class="flex h-full w-full items-center justify-center gap-2 bg-front px-8 py-3 text-center text-nowrap"
+                class="w-full transition-transform"
+                :class="{
+                    'group-hover:-translate-y-full group-focus-visible:-translate-y-full': !disabled,
+                }"
             >
-                <slot />
+                <span
+                    class="flex h-full w-full items-center justify-center gap-2 px-8 py-3 text-center text-nowrap"
+                    :class="disabled ? 'bg-front text-neutral-400' : 'bg-front'"
+                >
+                    <slot />
+                </span>
+                <span class="absolute top-full left-0 h-full w-full bg-theme-dark px-8 py-3">
+                    <UIcon
+                        name="heroicons:arrow-right-20-solid"
+                        class="absolute top-1/2 left-1/2 h-8 w-8 -translate-1/2 text-theme-white"
+                    />
+                </span>
             </span>
-            <span class="absolute top-full left-0 h-full w-full bg-theme-dark px-8 py-3">
-                <UIcon
-                    name="heroicons:arrow-right-20-solid"
-                    class="absolute top-1/2 left-1/2 h-8 w-8 -translate-1/2 text-theme-white"
-                />
-            </span>
-        </span>
-    </component>
+        </component>
+        <span
+            v-if="disabled"
+            class="absolute top-0 -left-4 -translate-y-1/2 -rotate-5 rounded-full bg-error px-4 py-1 text-sm whitespace-nowrap text-theme-white"
+            >{{ $t("menu.comingSoon") }}</span
+        >
+    </div>
 </template>
