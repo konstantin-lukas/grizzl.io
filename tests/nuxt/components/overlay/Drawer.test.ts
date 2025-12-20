a80b6eef17d30d6ce2a4b4fa93cb7445127e3eaa
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-12-20 15:16:54 +0100

    tests(components): add tests for OverlaySlideover component

--- tests/nuxt/components/overlay/Drawer.test.ts
+++ tests/nuxt/components/overlay/Drawer.test.ts
@@ -2,12 +2,12 @@ import Drawer from "@@/app/components/overlay/Drawer.vue";
 import { mountSuspended } from "@nuxt/test-utils/runtime";
 import { expect, test } from "vitest";
 
-const content = "Hello, world!";
+const slots = { default: "Hello, world!" };
+
 test("should be closed by default", async () => {
     await mountSuspended(Drawer, {
         scoped: true,
-        slots: { default: content },
-        props: { "data-test-id": "drawer" },
+        slots,
     });
     const drawer = document.querySelector("[role='dialog']");
     expect(drawer).toBeNull();
@@ -16,9 +16,9 @@ test("should be closed by default", async () => {
 test("should be opened by passing the open prop", async () => {
     await mountSuspended(Drawer, {
         scoped: true,
-        slots: { default: content },
-        props: { "data-test-id": "drawer", "open": true },
+        slots,
+        props: { open: true },
     });
     const drawer = document.querySelector("[role='dialog']");
-    expect(drawer?.textContent).toBe(content);
+    expect(drawer?.textContent).toBe(slots.default);
 });
