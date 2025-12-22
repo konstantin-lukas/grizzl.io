c23e4254b6b16c4718a7ff9f72b626a2ac8b1999
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-12-23 00:04:42 +0100

    test(e2e): add tests for pausing and resuming timer playback

--- tests/e2e/spec/timer/playback.test.ts
+++ tests/e2e/spec/timer/playback.test.ts
@@ -92,33 +92,62 @@ test("shows indicators for beats on timer progress and information on timer and 
     });
 });
 
-test("allows pausing and resuming timer playback", async ({ timerPage, db }) => {
+test("allows pausing, resuming, and resetting timer playback", async ({ timerPage, db }) => {
     const [timer] = await db.timer.insert({ count: 1 });
     await db.timerInterval.insert(timer.id, {
         count: 1,
         duration: 45000,
     });
 
-    await timerPage.page.clock.install({ time: new Date("2024-02-02T08:00:00") });
-    await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:00"));
-    await timerPage.goto();
-    await timerPage.click("listItemPlayButtons");
+    await test.step("Let timer run for some time, then pause and check remaining times", async () => {
+        await timerPage.page.clock.install({ time: new Date("2024-02-02T07:00:00") });
+        await timerPage.goto();
+        await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:00"));
+        await timerPage.click("listItemPlayButtons");
 
-    await timerPage.expect("remainingTime").toHaveText("01:30");
-    await timerPage.expect("remainingIntervalTime").toHaveText("00:45");
+        await timerPage.expect("remainingTime").toHaveText("01:30");
+        await timerPage.expect("remainingIntervalTime").toHaveText("00:45");
 
-    await timerPage.click("playButton");
-    await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:20"));
-    await timerPage.click("pauseButton");
-    await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:40"));
+        await timerPage.click("playButton");
+        await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:20"));
+        await timerPage.click("pauseButton");
+        await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:40"));
 
-    await timerPage.expect("remainingTime").toHaveText("01:10");
-    await timerPage.expect("remainingIntervalTime").toHaveText("00:25");
+        await timerPage.expect("remainingTime").toHaveText("01:10");
+        await timerPage.expect("remainingIntervalTime").toHaveText("00:25");
+    });
 
-    await timerPage.click("playButton");
-    await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:55"));
-    await timerPage.click("pauseButton");
+    await test.step("Resume timer, let it run some more, then pause and check remaining times", async () => {
+        await timerPage.click("playButton");
+        await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:00:55"));
+        await timerPage.click("pauseButton");
 
-    await timerPage.expect("remainingTime").toHaveText("00:55");
-    await timerPage.expect("remainingIntervalTime").toHaveText("00:10");
+        await timerPage.expect("remainingTime").toHaveText("00:55");
+        await timerPage.expect("remainingIntervalTime").toHaveText("00:10");
+    });
+
+    await test.step("Reset timer playback and check that remaining times are reset", async () => {
+        await timerPage.click("resetButton");
+
+        await timerPage.expect("remainingTime").toHaveText("01:30");
+        await timerPage.expect("remainingIntervalTime").toHaveText("00:45");
+    });
+
+    await test.step("Start timer, let it run again, then pause and check remaining times", async () => {
+        await timerPage.click("playButton");
+        await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:01:00"));
+        await timerPage.click("pauseButton");
+
+        await timerPage.expect("remainingTime").toHaveText("01:25");
+        await timerPage.expect("remainingIntervalTime").toHaveText("00:40");
+        await timerPage.expect("activeRound").toHaveText("1/2");
+    });
+
+    await test.step("Let timer run until the next interval, then check that displayed round is correct", async () => {
+        await timerPage.click("playButton");
+        await timerPage.page.clock.pauseAt(new Date("2024-02-02T08:01:39"));
+        await timerPage.page.clock.resume();
+
+        await timerPage.expect("activeRound").toHaveText("2/2");
+    });
 });
