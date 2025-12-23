039e8a2eafc2fca574735426648437e85803c3a3
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-12-23 16:22:58 +0100

    fix: try new client-side middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,12 +2,17 @@ import { PROTECTED_PATHS } from "#shared/constants/auth";
 import { authClient } from "~~/lib/auth-client";
 
 export default defineNuxtRouteMiddleware(async to => {
-    const { data: session } = await authClient.useSession(useFetch);
     const nuxtApp = useNuxtApp();
     if (import.meta.server || (import.meta.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered)) {
         return;
     }
-    if (!session.value && PROTECTED_PATHS.some(path => to.path.startsWith(path))) {
+    const headers = useRequestHeaders();
+    const { data: session } = await authClient.getSession({
+        fetchOptions: {
+            headers,
+        },
+    });
+    if (!session && PROTECTED_PATHS.some(path => to.path.startsWith(path))) {
         return navigateTo("/signin");
     }
 });
