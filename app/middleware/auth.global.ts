339aa57cd795fc96ae6dadfd5dfe1a06a8f39b63
Konstantin Lukas <kontakt@konstantinlukas.de>
2025-12-28 15:12:26 +0100

    fix: redirect to landing page if logged in and trying to open signin page

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -6,12 +6,18 @@ export default defineNuxtRouteMiddleware(async to => {
     if (import.meta.server || (import.meta.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered)) {
         return;
     }
+
     const headers = useRequestHeaders();
     const { data: session } = await authClient.getSession({
         fetchOptions: {
             headers,
         },
     });
+
+    if (session && to.path === "/signin") {
+        return navigateTo("/");
+    }
+
     if (!session && PROTECTED_PATHS.some(path => to.path.startsWith(path))) {
         return navigateTo("/signin");
     }
