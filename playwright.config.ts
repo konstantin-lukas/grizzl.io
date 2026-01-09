283124ee6ea6704b351683cdaa281d56d4c2607f
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-01-09 15:53:04 +0100

    chore(ci): update screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig<ConfigOptions>({
     expect: {
         timeout: 20000,
         toHaveScreenshot: {
-            maxDiffPixelRatio: 0,
+            maxDiffPixelRatio: 0.02,
             animations: "disabled",
         },
     },
