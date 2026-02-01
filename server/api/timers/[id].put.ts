fa64b0046e83a36fcced282d5c7a8caddad96991
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-02-01 14:02:11 +0100

    refactor: move timer query logic to timer repo

--- server/api/timers/[id].put.ts
+++ server/api/timers/[id].put.ts
@@ -1,10 +1,12 @@
 import { PutTimerSchema } from "#shared/validators/timer";
-import update from "~~/server/query/timer/update";
+import { db } from "~~/server/database";
+import TimerRepository from "~~/server/repositories/timer.repository";
 
 export default defineEventHandler(async event => {
     const id = await parseIdParameter(event);
     const body = await parseRequestBody(event, PutTimerSchema);
-    const rowCount = await tryThrow(update(id, event.context.user.id, body));
+    const timerRepository = new TimerRepository(db);
+    const rowCount = await tryThrow(timerRepository.update(id, event.context.user.id, body));
     if (rowCount === 0) throwError("The provided ID was not found.", "NOT_FOUND");
     setStatus(event, "NO_CONTENT");
 });
--- server/api/timers/index.post.ts
+++ server/api/timers/index.post.ts
@@ -1,10 +1,12 @@
 import { PostTimerSchema } from "#shared/validators/timer";
-import insert from "~~/server/query/timer/insert";
+import { db } from "~~/server/database";
+import TimerRepository from "~~/server/repositories/timer.repository";
 import { parseRequestBody } from "~~/server/utils/schema";
 
 export default defineEventHandler(async event => {
     const timer = await parseRequestBody(event, PostTimerSchema);
-    const data = await tryThrow(insert(event.context.user.id, timer));
+    const timerRepository = new TimerRepository(db);
+    const data = await tryThrow(timerRepository.create(event.context.user.id, timer));
     setStatus(event, "CREATED");
     setHeader(event, "Location", `/api/timers/${data}`);
 });
