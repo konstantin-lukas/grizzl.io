2da796b5fe7f9630e553070c31503d79a6c17e09
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-02-21 16:05:49 +0100

    test: allow some error margin in timer playback test screenshots

--- test-utils/playwright/pages/base.page.ts
+++ test-utils/playwright/pages/base.page.ts
@@ -112,9 +112,20 @@ export default abstract class BasePage<T extends Record<string, string>> {
 
     private async toHaveScreenshot(
         target: Locator | Page,
-        options: { name: string; blur?: boolean; threshold?: number },
+        options: {
+            name: string;
+            blur?: boolean;
+            threshold?: number;
+            maxDiffPixels?: number;
+            maxDiffPixelRatio?: number;
+        },
     ) {
-        const { blur = true, threshold = 0 } = options;
+        const {
+            blur = true,
+            threshold = config.expect?.toHaveScreenshot?.threshold,
+            maxDiffPixels = config.expect?.toHaveScreenshot?.maxDiffPixels,
+            maxDiffPixelRatio = config.expect?.toHaveScreenshot?.maxDiffPixelRatio,
+        } = options;
         const name = options.name.endsWith(".png") ? options.name : `${options.name}.png`;
         const fullPage = target === this.page;
 
@@ -128,8 +139,9 @@ export default abstract class BasePage<T extends Record<string, string>> {
             await this.page.click("body", { position: { x: 0, y: 0 } });
         }
 
-        if (name) return expect(target).toHaveScreenshot(name, { fullPage, threshold });
-        return expect(target).toHaveScreenshot({ fullPage, threshold });
+        if (name)
+            return expect(target).toHaveScreenshot(name, { fullPage, threshold, maxDiffPixels, maxDiffPixelRatio });
+        return expect(target).toHaveScreenshot({ fullPage, threshold, maxDiffPixels, maxDiffPixelRatio });
     }
 
     private async toBeAccessible() {
