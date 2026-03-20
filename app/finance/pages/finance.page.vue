cb71cec5560fb3c6df0cbd45d92ad92bb46741a1
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-03-20 17:41:32 +0100

    chore: add empty view

--- app/finance/pages/finance.page.vue
+++ app/finance/pages/finance.page.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-
+import Empty from "~/core/components/data/Empty.vue";
+import Wrapper from "~/core/components/layout/Wrapper.vue";
 </script>
 
 <template>
-    <div>Hello, world!</div>
+    <Wrapper :class="{ 'max-w-xl': true }">
+        <div class="flex min-h-main-height-no-padding w-full flex-col">
+            <div class="mb-16 flex w-full grow flex-col">
+                <Transition name="fade">
+                    <div v-if="true" class="center w-full grow">
+                        <Empty />
+                    </div>
+                </Transition>
+            </div>
+        </div>
+    </Wrapper>
 </template>
