d1765201c22f4e82f8ebc7c71a96b9d31b79e5de
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-04-21 20:17:05 +0200

    chore: mock model usage for tests

--- server/finance/api/finance/category-icon/index.get.ts
+++ server/finance/api/finance/category-icon/index.get.ts
@@ -3,7 +3,15 @@ import CategoryIconController from "#server/finance/controllers/category-icon.co
 export default defineEventHandler(async event => {
     const runtimeConfig = useRuntimeConfig();
     if (runtimeConfig.public.appEnv === "test") {
-        return { icon: "grocery" };
+        // This runtime logic was added to avoid using git lfs budget in CI workflow
+        const params = getQuery(event);
+        if (params["categoryName"] === undefined) {
+            throw createError({
+                statusCode: 400,
+            });
+        }
+        if (params["categoryName"]?.toString().startsWith("Dog")) return { icon: "pet-supplies-outline" };
+        return { icon: "question-mark-rounded" };
     }
     const categoryIconController = createContainer().resolve(CategoryIconController, event);
     return categoryIconController.getIconSuggestion(event);
