f1a2dc886e9809adf9b7c459a25e2b7a529bbb6b
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-04-21 20:07:32 +0200

    chore: skip usage of models in testing environments

--- server/finance/api/finance/category-icon/index.get.ts
+++ server/finance/api/finance/category-icon/index.get.ts
@@ -1,6 +1,10 @@
 import CategoryIconController from "#server/finance/controllers/category-icon.controller";
 
 export default defineEventHandler(async event => {
+    const runtimeConfig = useRuntimeConfig();
+    if (runtimeConfig.public.appEnv === "test") {
+        return { icon: "grocery" };
+    }
     const categoryIconController = createContainer().resolve(CategoryIconController, event);
     return categoryIconController.getIconSuggestion(event);
 });
