ce2335313df170c383253bd21be44293f867fc57
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-04-25 12:31:46 +0200

    feat: calculate expenses

--- app/finance/components/tabs/bills/BudgetsTab.vue
+++ app/finance/components/tabs/bills/BudgetsTab.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import H2 from "~/core/components/typo/H2.vue";
+import useToday from "~/core/composables/useToday";
+import usePerMonthTransactions from "~/finance/composables/usePerMonthTransactions";
+
+const { monthName } = useToday();
+usePerMonthTransactions();
 </script>
 
 <template>
-    <H2>{{ $t("finance") }}</H2>
-    <H2>Category Budgets For August</H2>
-    <H2>Expenses In Previous Months</H2>
+    <H2>{{ $t("finance.budgets.currentExpensesHeading", { month: monthName }) }}</H2>
+    <H2>{{ $t("finance.budgets.categoryBudgetsHeading", { month: monthName }) }}</H2>
+    <H2>{{ $t("finance.budgets.previousExpensesHeading") }}</H2>
 </template>
