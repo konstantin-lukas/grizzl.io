5f0a9eb0fe970aa1c93a05a14bfde56c5ccddcb9
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-04-25 22:46:36 +0200

    chore: post-review adjustments

--- app/finance/components/tabs/budgets/EmptyBudgets.vue
+++ app/finance/components/tabs/budgets/EmptyBudgets.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ICON_BILL } from "~/core/constants/icons.constant";
+import { ICON_WALLET } from "~/core/constants/icons.constant";
 </script>
 
 <template>
     <div class="flex w-full flex-col items-center justify-center rounded-xl bg-elevated">
         <UEmpty
             variant="naked"
-            :icon="ICON_BILL"
+            :icon="ICON_WALLET"
             :title="$t('ui.noEntries')"
             :description="$t('finance.budgets.noExpenses')"
             :ui="{ avatar: 'bg-default' }"
