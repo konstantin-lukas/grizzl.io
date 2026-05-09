5e6fdcd8884565a95a891f0436e90a13ec5377ca
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-05-09 11:27:43 +0200

    fix: don't show transaction loading skeletons when current transaction history is empty to avoid ui flickers

--- app/finance/components/tabs/AccountTab.vue
+++ app/finance/components/tabs/AccountTab.vue
@@ -39,7 +39,7 @@ watch(upsertFormOpen, isOpen => {
     <BalanceChart />
     <H2 class="mt-12">{{ $t("finance.account.transactionHistory") }}</H2>
     <div aria-live="polite">
-        <div v-if="isFetching">
+        <div v-if="isFetching && transactions.length > 0">
             <TransactionCardSkeleton v-for="n in 10" :key="n" />
         </div>
         <ul v-else class="relative min-h-32">
