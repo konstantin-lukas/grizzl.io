6c360ef4c1a93b64642a20c4001ae171e783111a
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-05-15 22:47:06 +0200

    chore: update failing tests

--- tests/e2e/navigation.test.ts
+++ tests/e2e/navigation.test.ts
@@ -2,7 +2,7 @@ import { test } from "~~/test-utils/playwright";
 import { withoutAuth } from "~~/test-utils/playwright/utils/auth";
 
 withoutAuth(() => {
-    const protectedPaths = ["/timer", "/finance"];
+    const protectedPaths = ["/timer", "/finance", "/todo"];
     for (const path of protectedPaths) {
         test(`redirects the user to ${path} after signing in through a client-side redirect`, async ({
             homePage: page,
