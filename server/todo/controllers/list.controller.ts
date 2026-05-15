39a423a4b8cee7384554aefd81eb6a1a976dfc77
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-05-15 12:54:13 +0200

    feat: add patch api endpoint for todo lists

--- server/todo/controllers/list.controller.ts
+++ server/todo/controllers/list.controller.ts
@@ -1,4 +1,5 @@
 import ListService from "#server/todo/services/list.service";
+import { DatabaseDeletedSchema } from "#shared/core/validators/core.validator";
 import { PostListSchema } from "#shared/todo/validators/list.validator";
 import type { H3Event } from "h3";
 import BaseController from "~~/server/core/controllers/base.controller";
@@ -13,6 +14,12 @@ export default class ListController extends BaseController {
 
     public async getCollection(event: H3Event) {
         return await this.listService.getCollection(event.context.user.id);
+    }
+
+    public async setDeletedStatus(event: H3Event) {
+        const id = ListController.parseIdParameter(event, "listId");
+        const body = await ListController.parseRequestBody(event, DatabaseDeletedSchema);
+        await this.listService.setDeletedStatus(id, event.context.user.id, body.deleted);
     }
 
     public async create(event: H3Event) {
