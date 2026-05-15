718eca64963cea6552ca1c0141b9e8aeef309912
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-05-15 13:32:59 +0200

    test: add integration tests for update todo list method

---
+++ tests/integration/todo/list.repository/update.test.ts
@@ -0,0 +1,25 @@
+import ListRepository from "~~/server/todo/repositories/list.repository";
+import { BASE_LIST } from "~~/test-utils/constants/todo";
+import { expect, test } from "~~/test-utils/vitest";
+
+let id = "";
+test.beforeEach(async ({ db, user }) => {
+    [{ id }] = await db.todoList.insert(5, { userId: user.id });
+});
+
+test("throws an error when the input data is faulty", async ({ db, user }) => {
+    const listRepository = new ListRepository(db.client);
+    await expect(listRepository.update(id, user.id, "" as never)).rejects.toThrow();
+});
+
+test("returns 0 when the given account id doesn't exist", async ({ db, user }) => {
+    const listRepository = new ListRepository(db.client);
+    const rows = await listRepository.update("bananas", user.id, BASE_LIST);
+    expect(rows).toBe(0);
+});
+
+test("returns 0 when the given user id doesn't exist", async ({ db }) => {
+    const listRepository = new ListRepository(db.client);
+    const rows = await listRepository.update(id, "bananas", BASE_LIST);
+    expect(rows).toBe(0);
+});
