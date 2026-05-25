525c4277418dafea927f520c9f59b82b848439e1
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-05-26 00:12:27 +0200

    refactor(todo): move all creation logic to actions service's create method

--- server/todo/services/action.service.ts
+++ server/todo/services/action.service.ts
@@ -9,6 +9,11 @@ import { tryCatch } from "#shared/core/utils/result.util";
 import type { CreateAction, PostActionQueue } from "#shared/todo/validators/action.validator";
 import { TODO_LIST_MAX_LENGTH } from "#shared/todo/validators/list.validator";
 
+interface MinimalList {
+    id: string;
+    items: { text: string; index: number | null; id: string }[];
+}
+
 export default class ActionService {
     static readonly deps = [ListRepository, ListItemRepository];
 
@@ -17,13 +22,40 @@ export default class ActionService {
         private readonly listItemRepository: ListItemRepository,
     ) {}
 
-    private async create(action: CreateAction, tx: DatabaseTransaction) {
+    private async create(action: CreateAction, list: MinimalList, tx: DatabaseTransaction) {
+        if (list.items.length >= TODO_LIST_MAX_LENGTH) {
+            const message = "Cannot create new task. The given list is full.";
+            const logMessage = `Cannot create new task on list with id ${list.id}. List is full.`;
+            throw new EntityLimitError(message, logMessage);
+        }
+
+        const largestIndex = list.items.reduce(
+            (max: { index: number | null }, item: { index: number | null }) => {
+                if (item.index === null) return max;
+                if (max.index === null) return item;
+                return item.index > max.index ? item : max;
+            },
+            { index: null },
+        ).index;
+
+        if (
+            (largestIndex === null && action.index !== null && action.index !== 0) ||
+            (largestIndex !== null && action.index !== null && action.index > largestIndex + 1)
+        ) {
+            const message = `Cannot create new task at position ${action.index}. Out of bounds.`;
+            const logMessage = `Cannot create new task on list with id ${list.id} at position ${action.index}. Out of bounds.`;
+            throw new OutOfBoundsError(message, logMessage);
+        }
+
         if (action.index !== null) {
             await this.listItemRepository.incrementIndices(action.listId, action.index, tx);
         }
 
         const { error } = await tryCatch(this.listItemRepository.create(action, tx));
-        if (!error) return;
+        if (!error) {
+            list.items.push({ ...action });
+            return;
+        }
 
         throw new UniqueConstraintError(
             `Unable to create item with id ${action.id} on list with id ${action.listId}`,
@@ -36,36 +68,15 @@ export default class ActionService {
             await this.listRepository.advisoryLock(`execute-to-do-list-actions-${userId}`, tx);
             const lists = await this.listRepository.findByUserId(userId, tx);
             for (const action of actions) {
-                const list = lists.find(list => list.id === action.listId);
+                const list: MinimalList | undefined = lists.find(list => list.id === action.listId);
                 if (!list) {
                     const message = `Cannot ${action.action} task with id ${action.id} on list with id ${action.listId} for user with id ${userId}. List does not exist for given user.`;
                     const logMessage = `Cannot ${action.action} task on given to-do list.`;
                     throw new NotFoundError(message, logMessage);
                 }
                 if (action.action === "create") {
-                    if (list.items.length >= TODO_LIST_MAX_LENGTH) {
-                        const message = "Cannot create new task. The given list is full.";
-                        const logMessage = `Cannot create new task on list with id ${list.id}. List is full.`;
-                        throw new EntityLimitError(message, logMessage);
-                    }
-                    const largestIndex = list.items.reduce(
-                        (max: { index: number | null }, item: { index: number | null }) => {
-                            if (item.index === null) return max;
-                            if (max.index === null) return item;
-                            return item.index > max.index ? item : max;
-                        },
-                        { index: null },
-                    ).index;
-                    if (
-                        (largestIndex === null && action.index !== null && action.index !== 0) ||
-                        (largestIndex !== null && action.index !== null && action.index > largestIndex + 1)
-                    ) {
-                        const message = `Cannot create new task at position ${action.index}. Out of bounds.`;
-                        const logMessage = `Cannot create new task on list with id ${list.id} at position ${action.index}. Out of bounds.`;
-                        throw new OutOfBoundsError(message, logMessage);
-                    }
-                    list.items.push({ ...action, scheduledFor: null });
-                    await this.create(action, tx);
+                    await this.create(action, list, tx);
+                    continue;
                 }
             }
         });
