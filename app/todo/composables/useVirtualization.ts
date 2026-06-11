8c2e8f9aa6429b2b11386bd8ddee4f68d4a2542d
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-06-11 20:42:47 +0200

    test(todo): disable virtualization in tests

--- app/todo/composables/useVirtualization.ts
+++ app/todo/composables/useVirtualization.ts
@@ -1,28 +1,31 @@
 export default function useVirtualization(element: Ref<Element | null>, delayOnMounted = false) {
-    const isVisible = ref(false);
+    const config = useRuntimeConfig();
+    const isVisible = ref(config.public.appEnv === "test");
     let observer: IntersectionObserver | null = null;
 
-    onMounted(async () => {
-        if (delayOnMounted) {
-            await new Promise(r => {
-                setTimeout(r, 0);
+    if (config.public.appEnv !== "test") {
+        onMounted(async () => {
+            if (delayOnMounted) {
+                await new Promise(r => {
+                    setTimeout(r, 0);
+                });
+            }
+            observer = new IntersectionObserver(([entry]) => {
+                if (!entry) return;
+                isVisible.value = entry.isIntersecting;
             });
-        }
-        observer = new IntersectionObserver(([entry]) => {
-            if (!entry) return;
-            isVisible.value = entry.isIntersecting;
+
+            if (element.value) {
+                observer.observe(element.value);
+            }
         });
 
-        if (element.value) {
-            observer.observe(element.value);
-        }
-    });
-
-    onBeforeUnmount(() => {
-        if (observer && element.value) {
-            observer.unobserve(element.value);
-        }
-    });
+        onBeforeUnmount(() => {
+            if (observer && element.value) {
+                observer.unobserve(element.value);
+            }
+        });
+    }
 
     return isVisible;
 }
