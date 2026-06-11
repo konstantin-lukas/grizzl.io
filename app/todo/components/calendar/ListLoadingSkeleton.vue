0544d7fb121af2124bd260543d47ddb78f9b8ddc
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-06-11 20:48:29 +0200

    style(todo): update skeleton

--- app/todo/components/calendar/ListLoadingSkeleton.vue
+++ app/todo/components/calendar/ListLoadingSkeleton.vue
@@ -2,14 +2,18 @@
 
 <template>
     <div class="mt-16">
-        <div class="flex items-center gap-4">
+        <div class="mb-8 flex items-center gap-4">
             <USkeleton class="size-12 shrink-0 rounded-full" />
             <USkeleton class="h-8 w-full" />
         </div>
-        <USeparator class="mt-4" />
-        <div v-for="i in 10" class="mt-4 flex items-center gap-4" :key="i">
-            <USkeleton class="size-5 rounded-sm" />
-            <USkeleton class="h-4 w-full rounded-sm" :style="{ maxWidth: ((9 * (i + 1)) % 7) + 6 + 'rem' }" />
+        <div v-for="i in 3" :key="i" class="mt-6 flex items-center gap-4">
+            <USkeleton class="size-4.5 rounded-sm" />
+            <USkeleton class="h-3.5 w-full rounded-sm" :style="{ maxWidth: ((9 * (i + 1)) % 7) + 6 + 'rem' }" />
+        </div>
+        <USeparator class="mt-6" />
+        <div v-for="i in 2" :key="i" class="mt-6 flex items-center gap-4">
+            <USkeleton class="size-4.5 rounded-sm" />
+            <USkeleton class="h-3.5 w-full rounded-sm" :style="{ maxWidth: ((9 * (i + 1)) % 7) + 6 + 'rem' }" />
         </div>
     </div>
 </template>
