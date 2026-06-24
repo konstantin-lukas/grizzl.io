5dae7cd557f717a8e85aaf923a4852bfabd08154
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-06-24 21:38:34 +0200

    fix(poll): adjust failing test

--- tests/api/polls/get.test.ts
+++ tests/api/polls/get.test.ts
@@ -1,3 +1,4 @@
+import { expect, test } from "~~/test-utils/playwright";
 import { makePollTestBuilder } from "~~/test-utils/playwright/builders/poll";
 
 const testBuilder = makePollTestBuilder("get-collection");
@@ -6,6 +7,32 @@ testBuilder
     .returnsA401StatusCodeWhenAnUnauthenticatedRequestIsMade()
     .returnsAnEmptyArrayWhenThereAreNoResources()
     .doesNotReturnResourcesOfOtherUsers()
-    .allowsRetrievingAListOfResourcesSortedBy()
     .doesNotReturnSoftDeletedResources()
     .build();
+
+test("allows retrieving a list of resources sorted by creation date", async ({ request, db }) => {
+    await db.poll.insert(1, { choices: ["A", "B"], title: "Letter", createdAt: new Date("2025-01-01") });
+    await db.poll.insert(1, { choices: ["A", "B"], title: "Letter", createdAt: new Date("2026-01-01") });
+    const response = await request.get("/api/polls");
+
+    expect(await response.json()).toStrictEqual([
+        expect.objectContaining({
+            choices: ["A", "B"],
+            closesAt: null,
+            createdAt: "2026-01-01T00:00:00.000Z",
+            majorityWinner: false,
+            method: "positional",
+            title: "Letter",
+            votes: [],
+        }),
+        expect.objectContaining({
+            choices: ["A", "B"],
+            closesAt: null,
+            createdAt: "2025-01-01T00:00:00.000Z",
+            majorityWinner: false,
+            method: "positional",
+            title: "Letter",
+            votes: [],
+        }),
+    ]);
+});
