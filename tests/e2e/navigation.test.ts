e1595ed2bceceae429b3b566152b6b0a8f4bab5d
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-06-24 21:46:03 +0200

    fix(poll): adjust failing test

--- tests/e2e/navigation.test.ts
+++ tests/e2e/navigation.test.ts
@@ -2,22 +2,30 @@ import { test } from "~~/test-utils/playwright";
 import { withoutAuth } from "~~/test-utils/playwright/utils/auth";
 
 withoutAuth(() => {
-    test(`redirects the user to /timer after signing in through a client-side redirect`, async ({ homePage: page }) => {
-        await page.goto();
-        await page.click("menuButton");
-        await page.click("timerLink");
-        await page.expect().toHaveURL("/signin?callbackURL=/timer");
-        await page.page.getByTestId("keycloak-provider").click();
-        await page.page.locator("#username").fill("user");
-        await page.page.locator("#password").fill("password");
-        await page.page.locator("#kc-login").click();
-        await page.expect().toHaveURL("/timer");
-    });
+    for (const [path, link] of [
+        ["/timer", "timerLink"],
+        ["/finance", "financeLink"],
+        ["/todo", "todoLink"],
+        ["/poll", "pollLink"],
+    ] as const) {
+        test(`redirects the user to ${path} after signing in through a client-side redirect`, async ({
+            homePage: page,
+        }) => {
+            await page.goto({ target: path });
+            await page.click("menuButton");
+            await page.click(link);
+            await page.expect().toHaveURL(`/signin?callbackURL=${path}`);
+            await page.page.getByTestId("keycloak-provider").click();
+            await page.page.locator("#username").fill("user");
+            await page.page.locator("#password").fill("password");
+            await page.page.locator("#kc-login").click();
+            await page.expect().toHaveURL(path);
+        });
+    }
 
-    const protectedPaths = ["/timer", "/finance", "/todo", "/todo/calendar", "/poll"];
-    for (const path of protectedPaths) {
+    for (const path of ["/timer", "/finance", "/todo", "/todo/calendar", "/poll"]) {
         test(`redirects the user to ${path} after signing in through a server-side redirect`, async ({
-            timerPage: page,
+            homePage: page,
         }) => {
             await page.goto({ target: path });
             await page.expect().toHaveURL(`/signin?callbackURL=${path}`);
