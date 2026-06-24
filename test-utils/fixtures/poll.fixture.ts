321335521c73ece941543a504866ca8ee214ffbe
Konstantin Lukas <kontakt@konstantinlukas.de>
2026-06-24 18:58:29 +0200

    refactor(poll): fix type issue

--- test-utils/fixtures/poll.fixture.ts
+++ test-utils/fixtures/poll.fixture.ts
@@ -3,12 +3,12 @@ import { MAX_POLL_CHOICES } from "#shared/poll/validators/poll.validator";
 import { PollMethod, VoterIdentityMethod } from "../../shared/poll/enums/method.enum";
 import BaseFixture from "@@/test-utils/fixtures/base.fixture";
 import type { Database } from "~~/database";
-import { boolean, date, enumValue, str, strArr } from "~~/test-utils/helpers/data";
+import { boolean, date, enumValue, maybe, str, strArr } from "~~/test-utils/helpers/data";
 
 export default class PollFixture extends BaseFixture<"poll"> {
     protected defaults = (seed: number) => ({
         title: str({ length: LONG_TITLE_MAX, seed }),
-        closesAt: date({ seed }),
+        closesAt: maybe(() => date({ seed }), { seed }),
         choices: strArr({ arrLength: MAX_POLL_CHOICES, seed, strLength: LIST_MAX }),
         method: enumValue(PollMethod, { seed }),
         voterIdentityMethod: enumValue(VoterIdentityMethod, { seed }),
